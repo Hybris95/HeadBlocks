56d737902eaedced275b06f24ffd6e38e4c5d26e
Socolin <beber-le-scaraber@hotmail.fr>
2011-04-10 23:03:43 +0200

    Data field

--- src/com/hybris/bukkit/headblocks/HeadBlocksExecutor.java
+++ src/com/hybris/bukkit/headblocks/HeadBlocksExecutor.java
@@ -9,77 +9,89 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 class HeadBlocksExecutor implements CommandExecutor{
-	
-	private HeadBlocks plugin;
-	
-	HeadBlocksExecutor(HeadBlocks plugin){
-		this.plugin = plugin;
-	}
-	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-		
-		if(label.equals("hb")){
-			
-			if(args.length <= 1){return false;}
-			if(!args[0].equalsIgnoreCase("self") && !args[0].equalsIgnoreCase("s") && !args[0].equalsIgnoreCase("other") && !args[0].equalsIgnoreCase("o")){return false;}
-			
-			if(args[0].equalsIgnoreCase("self") || args[0].equalsIgnoreCase("s")){
-				// Self - Only if a Player
-				if(sender instanceof Player){
-					Player player = (Player)sender;
-					String idOrMaterial = args[1];
-					Material material = idParse(idOrMaterial);
-					if(material == null){return false;}
-					
-					if(plugin.hasPermissions(sender, "self")){
-						// TODO Recuperer l'ancien et le mettre dans l'inventaire (si possible - sinon abandonner et return true)
-						player.getInventory().setHelmet(new ItemStack(material, 1, (short)1, (byte)0)); // TODO Permettre de changer la couleur de la laine par exemple
-						return true;
-					}
-					else{return false;}
-				}
-				else{return false;}
-			}
-			else if(args[0].equalsIgnoreCase("other") || args[0].equalsIgnoreCase("o")){
-				if(args.length <= 2){return false;}
-				
-				String playerName = args[1];
-				Player player = plugin.getServer().getPlayer(playerName);
-				if(player == null){return false;}
-				
-				String idOrMaterial = args[2];
-				Material material = idParse(idOrMaterial);
-				if(material == null){return false;}
-				
-				if(plugin.hasPermissions(sender, "other")){
-					// TODO Recuperer l'ancien et le mettre dans l'inventaire (si possible - sinon abandonner et return true)
-					player.getInventory().setHelmet(new ItemStack(material, 1, (short)1, (byte)0)); // TODO Permettre de changer la couleur de la laine par exemple
-					return true;
-				}
-				else{return false;}
-			}
-			else{
-				return false; // Should never happen
-			}
-			
-		}
-		else{return false;} // Should never happen
-		
-	}
-	
-	private Material idParse(String idOrMaterial){
-		Material material = null;
-		int id = -1;
-		try{
-			id = Integer.parseInt(idOrMaterial);
-			material = Material.getMaterial(id);
-		}
-		catch(NumberFormatException e){}
-		
-		if(material == null){
-			material = Material.matchMaterial(idOrMaterial);
-		}
-		return material;
-	}
-	
+
+    private HeadBlocks plugin;
+
+    HeadBlocksExecutor(HeadBlocks plugin){
+        this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+
+        if(label.equals("hb")){
+
+            if(args.length <= 1){return false;}
+            if(!args[0].equalsIgnoreCase("self") && !args[0].equalsIgnoreCase("s") && !args[0].equalsIgnoreCase("other") && !args[0].equalsIgnoreCase("o")){return false;}
+
+            if(args[0].equalsIgnoreCase("self") || args[0].equalsIgnoreCase("s")){
+                // Self - Only if a Player
+                int dmg = 0;
+                if(sender instanceof Player){
+                    Player player = (Player)sender;
+                    String idOrMaterial = args[1];
+                    if (args[1].contains(":")) {
+                        String[] parts = name.split(":");
+                        String data = parts[1];
+                        idOrMaterial = parts[0];
+                        try {
+                             dmg = Integer.parseInt(data);
+                        } catch (NumberFormatException e) {
+                            dmg = 0;
+                            // TODO: Convertir texte en valeur
+                        }
+                    }
+                    Material material = idParse(idOrMaterial);
+                    if(material == null){return false;}
+
+                    if(plugin.hasPermissions(sender, "self")){
+                        // TODO Recuperer l'ancien et le mettre dans l'inventaire (si possible - sinon abandonner et return true)
+                        player.getInventory().setHelmet(new ItemStack(material, 1, (short)dmg, (byte)dmg)); // TODO Permettre de changer la couleur de la laine par exemple
+                        return true;
+                    }
+                    else{return false;}
+                }
+                else{return false;}
+            }
+            else if(args[0].equalsIgnoreCase("other") || args[0].equalsIgnoreCase("o")){
+                if(args.length <= 2){return false;}
+
+                String playerName = args[1];
+                Player player = plugin.getServer().getPlayer(playerName);
+                if(player == null){return false;}
+
+                String idOrMaterial = args[2];
+                Material material = idParse(idOrMaterial);
+                if(material == null){return false;}
+
+                if(plugin.hasPermissions(sender, "other")){
+                    // TODO Recuperer l'ancien et le mettre dans l'inventaire (si possible - sinon abandonner et return true)
+                    player.getInventory().setHelmet(new ItemStack(material, 1, (short)1, (byte)0)); // TODO Permettre de changer la couleur de la laine par exemple
+                    return true;
+                }
+                else{return false;}
+            }
+            else{
+                return false; // Should never happen
+            }
+
+        }
+        else{return false;} // Should never happen
+
+    }
+
+    private Material idParse(String idOrMaterial){
+        Material material = null;
+        int id = -1;
+        try{
+            id = Integer.parseInt(idOrMaterial);
+            material = Material.getMaterial(id);
+        }
+        catch(NumberFormatException e){}
+
+        if(material == null){
+            material = Material.matchMaterial(idOrMaterial);
+        }
+        return material;
+    }
+
 }
