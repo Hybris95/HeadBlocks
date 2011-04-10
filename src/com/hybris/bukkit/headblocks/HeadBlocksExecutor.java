dcb63328cbf1f675b0f5d0bac784f8a8cc22b720
Socolin <beber-le-scaraber@hotmail.fr>
2011-04-11 00:44:42 +0200

    select data using name

--- src/com/hybris/bukkit/headblocks/HeadBlocksExecutor.java
+++ src/com/hybris/bukkit/headblocks/HeadBlocksExecutor.java
@@ -30,19 +30,24 @@ class HeadBlocksExecutor implements CommandExecutor{
 
                     byte dataByte = 0;
                     String idOrMaterial = args[1];
+                    Material material = null;
                     if (idOrMaterial.contains(":")) {
                         String[] parts = idOrMaterial.split(":");
-                        String data = parts[1];
                         idOrMaterial = parts[0];
+                        material = idParse(idOrMaterial);
+                        String data = dataParse(parts[1],material.getId() );
                         try {
                              dataByte = Byte.parseByte(data);
                         } catch (NumberFormatException e) {
                             dataByte = 0;
                         }
                     }
-                    Material material = idParse(idOrMaterial);
-                    if(material == null){return false;}
+                    else
+                        material = idParse(idOrMaterial);
 
+                    if(material == null){return false;}
+                    if(!material.isBlock())//TODO Message d'erreur
+                        {return false;}
                     if(plugin.hasPermissions(sender, "self")){
                         // TODO Recuperer l'ancien et le mettre dans l'inventaire (si possible - sinon abandonner et return true)
                         player.getInventory().setHelmet(new ItemStack(material, 1, (short)1, (byte)dataByte));
@@ -61,21 +66,24 @@ class HeadBlocksExecutor implements CommandExecutor{
 
                 byte dataByte = 0;
                 String idOrMaterial = args[2];
+                Material material = null;
                 if (idOrMaterial.contains(":")) {
                     String[] parts = idOrMaterial.split(":");
-                    String data = parts[1];
                     idOrMaterial = parts[0];
+                    material = idParse(idOrMaterial);
+                    String data = dataParse(parts[1],material.getId() );
                     try {
                          dataByte = Byte.parseByte(data);
                     } catch (NumberFormatException e) {
                         dataByte = 0;
                     }
                 }
+                else
+                    material = idParse(idOrMaterial);
 
-                String idOrMaterial = args[2];
-                Material material = idParse(idOrMaterial);
                 if(material == null){return false;}
-
+                if(!material.isBlock())//TODO Message d'erreur
+                        {return false;}
                 if(plugin.hasPermissions(sender, "other")){
                     // TODO Recuperer l'ancien et le mettre dans l'inventaire (si possible - sinon abandonner et return true)
                     player.getInventory().setHelmet(new ItemStack(material, 1, (short)1, (byte)dataByte)); // TODO Permettre de changer la couleur de la laine par exemple
@@ -105,6 +113,69 @@ class HeadBlocksExecutor implements CommandExecutor{
             material = Material.matchMaterial(idOrMaterial);
         }
         return material;
+    }
+
+    private byte dataParse(String data,int id)
+    {
+        try {
+             return Byte.parseByte(data);
+        } catch (NumberFormatException e) {
+            switch (id)
+            {
+                case 17:
+                case 18:
+                    if (data.equalsIgnoreCase("redwood")) {
+                        return 1;
+                    } else if (data.equalsIgnoreCase("birch") || data.equalsIgnoreCase("bouleau")) {
+                        return 2;
+                    }
+                    return 0;
+                case 43:
+                case 44:
+                    if (data.equalsIgnoreCase("sandstone")) {
+                        return 1;
+                    } else if (data.equalsIgnoreCase("wood") {
+                        return 2;
+                    } else if (data.equalsIgnoreCase("cobble") {
+                        return 3;
+                    }
+                    return 0;
+                case 35:
+                    if (data.equalsIgnoreCase("orange")) {
+                        return 1;
+                    } else if (data.equalsIgnoreCase("magenta") {
+                        return 2;
+                    } else if (data.equalsIgnoreCase("lightblue") {
+                        return 3;
+                    } else if (data.equalsIgnoreCase("yellow") {
+                        return 4;
+                    } else if (data.equalsIgnoreCase("lightgreen") {
+                        return 5;
+                    } else if (data.equalsIgnoreCase("pink") {
+                        return 6;
+                    } else if (data.equalsIgnoreCase("grey") {
+                        return 7;
+                    } else if (data.equalsIgnoreCase("lightgrey") {
+                        return 8;
+                    } else if (data.equalsIgnoreCase("cyan") {
+                        return 9;
+                    } else if (data.equalsIgnoreCase("purple") {
+                        return 10;
+                    } else if (data.equalsIgnoreCase("blue") {
+                        return 11;
+                    } else if (data.equalsIgnoreCase("brown") {
+                        return 12;
+                    } else if (data.equalsIgnoreCase("green") {
+                        return 13;
+                    } else if (data.equalsIgnoreCase("red") {
+                        return 14;
+                    } else if (data.equalsIgnoreCase("black") {
+                        return 15;
+                    }
+                    return 0;
+            }
+            dataByte = 0;
+        }
     }
 
 }
